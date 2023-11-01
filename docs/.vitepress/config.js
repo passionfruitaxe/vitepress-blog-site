6b611b952631a9eca7ab4e62c54b09c9746ddd9d
luowei <86441933@qq.com>
2023-11-01 11:21:34 +0800

    style: 更换code主题

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -15,7 +15,7 @@ export default {
   lastUpdated: true,
   head: [["link", { rel: "icon", href: "/favicon.ico" }]],
   markdown: {
-    theme: "one-dark-pro",
+    theme: "material-palenight",
     lineNumbers: true,
   },
   themeConfig: {
